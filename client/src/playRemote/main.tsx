60fd074dc2ca83a72d2bc82134d5c1611addbcf2
Brahim Amzil <bramzil@e2r9p15.1337.ma>
2025-12-13 13:30:32 +0100

    prepare pong service to integrate data base

--- client/src/playRemote/main.tsx
+++ client/src/playRemote/main.tsx
@@ -6,13 +6,22 @@ export function RemoteOptions() {
   return (
     <Menu>
       <MenuOption>
-        <Link to="/dashboard/games/pingpong/remote/someone">Play Match</Link>
+        <Link 
+          to="/dashboard/games/pingpong/remote/someone"
+          className='w-full h-full'
+        >Play Match</Link>
       </MenuOption>
       <MenuOption>
-        <Link to="/pongGame/remote/invitefriend">Invite Friend</Link>
+        <Link
+          to="/pongGame/remote/invitefriend"
+          className='w-full h-full'
+        >Invite Friend</Link>
       </MenuOption>
       <MenuOption>
-        <Link to="/pongGame">Go Back</Link>
+        <Link
+          to="/pongGame"
+          className='w-full h-full'
+        >Go Back</Link>
       </MenuOption>
     </Menu>
   );
