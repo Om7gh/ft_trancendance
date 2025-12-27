a915f1fbbca2fc9dcb6713c38bdc796c70e0eac5
aaitelka <aaitelka@student.1337.ma>
2025-12-27 23:21:10 +0100

    fix(logout): add separate function to revoke refresh token and integrate this changes with client, server/logout endpoint just clear accessToken and refresh/revoke to clear refreshToken

--- services/identity-server/src/controllers/TokenController.ts
+++ services/identity-server/src/controllers/TokenController.ts
@@ -1,33 +1,62 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 
 export default abstract class TokenController {
-  static async generate(
-    this: FastifyInstance,
-    request: FastifyRequest,
-    reply: FastifyReply
-  ) {
-    const token = request.cookies.refreshToken;
-    if (!token) {
-      reply.clearAccessToken();
-      return reply.unauthorized('loggin first');
+    static async generate(
+        this: FastifyInstance,
+        request: FastifyRequest,
+        reply: FastifyReply
+    ) {
+        const token = request.cookies.refreshToken;
+        if (!token) {
+            reply.clearAccessToken();
+            return reply.unauthorized('loggin first');
+        }
+        try {
+            const payload = await request.verifyRefreshToken();
+            const user = this.usersRepository.findByUID(payload.sub!);
+            if (payload.jti != user?.token_id) {
+                return reply
+                    .clearAccessToken()
+                    .clearRefreshToken()
+                    .forbidden('refresh token rotated');
+            }
+            if (!user) {
+                return reply.forbidden(
+                    'you dont have access to this resources'
+                );
+            }
+            const newAccessToken = await this.generateAccessToken(payload.sub!);
+            return reply
+                .sendAccessToken(newAccessToken)
+                .send({ success: true });
+        } catch (err) {
+            reply.clearAccessToken().clearRefreshToken();
+            return reply.unauthorized('refresh token invalid');
+        }
     }
-    try {
-      const payload = await request.verifyRefreshToken();
-      const user = this.usersRepository.findByUID(payload.sub!);
-      if (payload.jti != user?.token_id) {
-        return reply
-          .clearAccessToken()
-          .clearRefreshToken()
-          .forbidden('refresh token rotated');
-      }
-      if (!user) {
-        return reply.forbidden('you dont have access to this resources');
-      }
-      const newAccessToken = await this.generateAccessToken(payload.sub!);
-      return reply.sendAccessToken(newAccessToken).send({ success: true });
-    } catch (err) {
-      reply.clearAccessToken().clearRefreshToken();
-      return reply.unauthorized('refresh token invalid');
+
+    static async revoke(
+        this: FastifyInstance,
+        request: FastifyRequest,
+        reply: FastifyReply
+    ) {
+        const refreshToken = request.cookies.refreshToken;
+
+        if (refreshToken) {
+            try {
+                const payload = await request.verifyRefreshToken();
+                const user = this.usersRepository.findByUID(payload.sub!);
+                if (!user) {
+                    return reply.badRequest();
+                }
+                this.usersRepository.update(user.id, {
+                    token_id: '',
+                });
+                console.log(`Token revoked for user ${payload.sub}`);
+            } catch (err) {
+                console.error('Error revoking token:', err);
+            }
+        }
+        return reply.clearRefreshToken().send({ success: true });
     }
-  }
 }
