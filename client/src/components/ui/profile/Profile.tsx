6fea19b9c6943aea75753d053d4dbea34b268996
Ilyasse <ilyassealdidi@gmail.com>
2025-12-28 16:35:05 +0100

    fix: update profile display to show user's full name dynamically

--- client/src/components/ui/profile/Profile.tsx
+++ client/src/components/ui/profile/Profile.tsx
@@ -52,7 +52,7 @@ export default function Profile(): JSX.Element {
       {active && (
         <div className="absolute top-16 right-0 w-48 bg-slate-900  shadow-xl overflow-hidden animate-dropdown origin-top-right z-50">
           <div className="px-4 py-3 bg-violet-500">
-            <p className="text-white font-medium truncate">Omar Ghazi</p>
+            <p className="text-white font-medium truncate">{`${user?.first_name || ''} ${user?.last_name || ''}`.trim()}</p>
           </div>
           <ul className="divide-y divide-slate-300">
             <li className="hover:bg-slate-700 transition-colors">
